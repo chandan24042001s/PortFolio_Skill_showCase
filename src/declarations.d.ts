e6f5b4e567d46360acecf18f2e950fec5d8d933a
Chandan Kumal <85206406+chandan24042001s@users.noreply.github.com>
2023-12-17 21:11:58 +0530

    Completion

---
+++ src/declarations.d.ts
@@ -0,0 +1,6 @@
+declare module '@iconscout/react-unicons/icons/uil-linkedin';
+declare module '@iconscout/react-unicons/icons/uil-facebook';
+declare module '@iconscout/react-unicons/icons/uil-github';
+declare module '@iconscout/react-unicons/icons/uil-sun';
+declare module '@iconscout/react-unicons/icons/uil-moon';
+declare module '@iconscout/react-unicons/icons/uil-leetcode';
