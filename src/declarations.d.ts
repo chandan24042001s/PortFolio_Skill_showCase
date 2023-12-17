babefda47e97e439e8dbf448deefe14d883c4e01
Chandan Kumal <85206406+chandan24042001s@users.noreply.github.com>
2023-12-17 21:09:44 +0530

    Delete src directory

--- src/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@iconscout/react-unicons/icons/uil-linkedin';
-declare module '@iconscout/react-unicons/icons/uil-facebook';
-declare module '@iconscout/react-unicons/icons/uil-github';
-declare module '@iconscout/react-unicons/icons/uil-sun';
-declare module '@iconscout/react-unicons/icons/uil-moon';
