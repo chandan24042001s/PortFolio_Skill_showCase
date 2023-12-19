6b4c0565000dd1ff5a7d4311f22839e243995d37
Chandan <hb7102849@gmail.com>
2023-12-19 21:19:02 +0530

    Profile bug

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useRef } from "react";
 import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
+
 import LeetCode from "../../img/leetcode.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
@@ -85,7 +85,7 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <img src="" alt="" />
         {/* animation */}
         <motion.img
           initial={{ left: "-36%" }}
